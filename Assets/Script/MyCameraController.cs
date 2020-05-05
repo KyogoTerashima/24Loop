c1ca2c13df0e89db68ea2216ead06706ca2cb919
KyogoTerashima <kyogoterashima@outlook.jp>
2020-05-05 22:53:41 +0900

    個人製作　24Loop　配置データ作成

---
+++ Assets/Script/MyCameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour
+{
+
+    private GameObject playerCharacter;
+
+    private float difference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.playerCharacter = GameObject.Find("PlayerCharacter");
+        this.difference = this.playerCharacter.transform.position.x - this.transform.position.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.playerCharacter.transform.position.x - this.difference,  this.transform.position.y, this.transform.position.z);
+        
+    }
+}
---
+++ Assets/Script/PlayerCharacterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    //アニメーションのコンポーネント
+    private Animator myAnimator;
+    //移動のためのコンポーネントを入れる
+    private Rigidbody myRigdbody;
+    //前へと進む力
+    private float forwordForce = 100f;
+    //前へと進む力を減衰させる
+    private float coefficient = 0.75f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myAnimator = GetComponent<Animator>();
+        this.myAnimator.SetFloat("Speed_f", 0.1f);
+        //リジットの取得
+        this.myRigdbody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Wキーが押されたら前へと進む
+        if (Input.GetKey(KeyCode.W) && this.myAnimator.GetFloat("Speed_f") <= 3)
+        {
+            float Speed = this.myAnimator.GetFloat("Speed_f");
+            float mySpeed = Speed * this.forwordForce;
+            this.myAnimator.SetFloat("Speed_f",  mySpeed);
+            this.myRigdbody.AddForce(Speed, 0, 0);
+            
+            
+        }
+
+        //キーが離されたら前の動きを止める
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            this.myAnimator.SetFloat("Speed_f", 0.1f);
+            Debug.Log(this.myAnimator.GetFloat("Speed_f"));
+            this.myRigdbody.velocity = Vector3.zero;
+            Debug.Log(this.myRigdbody.velocity = Vector3.zero);
+        }
+    }
+}
